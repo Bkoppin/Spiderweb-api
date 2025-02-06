b62a11e3dec655b8e60dfebf1e0442b56a4fc9f4
larwis95 <156850505+larwis95@users.noreply.github.com>
2025-02-05 19:48:03 -0500

    chore: renamed some packages and folders and methods to better fit golang naming conventions

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	controller "api/internal/app/http"
-	"api/internal/app/router"
+	"api/internal/app/controller"
+	"api/internal/app/routing"
 )
 
 func main() {
 	// Create a new router
-	router := router.NewRouter()
+	router := routing.New()
 	// Add routes to the router
 	router.Handle("/api/v1/auth/login", controller.Test())
 
 	// Start the server on port 8080
-	router.Serve(8080)
+
+	router.Serve(8080, routing.ServeOptions{Message: "http://localhost:8080",})
 
 }
 
