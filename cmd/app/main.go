ec5ea9512314e723ee0819e1b234bd9ea7843dc9
larwis95 <156850505+larwis95@users.noreply.github.com>
2025-03-26 03:46:35 -0400

    feat(neo4j): created simple query builder to interface with neo4j database -implemented some restful api routes for creating a world, fetching a world, and fetching user worlds
    Need to cleanup implmentation a little bit, mostly just trying to figure out neo4j querying

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -13,7 +13,9 @@ func main() {
 	router.Handle("/api/users", controller.CreateUser, "POST")
 	router.Handle("/api/users/{id}", controller.GetUser, "GET")
 	router.Handle("/api/auth/login", controller.Login, "POST")
-	router.Handle("/api/world", controller.CreateWorld, "POST")
+	router.Handle("/api/users/{id}/world", controller.CreateWorld, "POST")
+	router.Handle("/api/users/{id}/worlds", controller.GetUserWorlds, "GET")
+	router.Handle("/api/world/{id}", controller.GetWorld, "GET")
 	router.Serve(8080, routing.ServeOptions{Message: "http://localhost:8080",})
 
 }
--- internal/app/controller/world.go
+++ internal/app/controller/world.go
@@ -1,55 +1,193 @@
 package controller
 
 import (
+	"api/internal/app/models"
 	neo "api/internal/app/neo4j"
 	"api/internal/app/routing"
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
-type World struct {
-	Name string `json:"name"`
-}
-
-func CreateWorld(
-	w http.ResponseWriter,
-	r *http.Request,
-	rctx routing.Context,
-) {
-
+func createWorld(query string, params map[string]interface{}) (*models.World, error) {
 	ctx := context.Background()
+	driver, err := neo.NewDriver()
+	if err != nil {
+		return nil, err
+	}
+	defer driver.Close(ctx)
 
-	var body struct {
-		Name   string `json:"name"`
+	session := driver.NewSession(ctx, neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	defer session.Close(ctx)
+
+	tx, err := session.BeginTransaction(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback(ctx)
+
+	res, err := tx.Run(ctx, query, params)
+	if err != nil {
+		return nil, err
 	}
 
-	err := json.NewDecoder(r.Body).Decode(&body)
+	record, err := res.Single(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	worldValue, ok := record.Get("w")
+	if !ok {
+		return nil, fmt.Errorf("failed to retrieve 'w' from record")
+	}
+	worldNode := worldValue.(neo4j.Node)
+	
+	world := models.World{
+		ID:					worldNode.ElementId,
+		Name:        worldNode.Props["name"].(string),
+		Type:        worldNode.Props["type"].(string),
+		Description: worldNode.Props["description"].(string),
+	}
+
+	err = tx.Commit(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+	
+	return &world, nil
+
+}
+
+func fetchWorld(query string, params map[string]interface{}) (*models.World, error) {
+	ctx := context.Background()
+	driver, err := neo.NewDriver()
+	if err != nil {
+		return nil, err
+	}
+	defer driver.Close(ctx)
+
+	session := driver.NewSession(ctx, neo4j.SessionConfig{AccessMode: neo4j.AccessModeRead})
+	defer session.Close(ctx)
+
+	res, err := session.Run(ctx, query, params)
+	if err != nil {
+		return nil, err
+	}
+
+	record, err := res.Single(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	worldValue, ok := record.Get("w")
+	if !ok {
+		return nil, fmt.Errorf("failed to retrieve 'w' from record")
+	}
+	worldNode := worldValue.(neo4j.Node)
+	
+	world := models.World{
+		ID:					worldNode.ElementId,
+		Name:        worldNode.Props["name"].(string),
+		Type:        worldNode.Props["type"].(string),
+		Description: worldNode.Props["description"].(string),
+	}
+
+	return &world, nil
+}
+
+
+func CreateWorld(w http.ResponseWriter, r *http.Request, rctx routing.Context) {
+	var world models.World
+
+	userID := rctx.GetPathParam("id")
+	if userID == "" {
+		http.Error(w, "missing id", http.StatusBadRequest)
+		return
+	}
+
+
+	err := json.NewDecoder(r.Body).Decode(&world)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	driver, err := neo.NewDriver()
+
+	queryBuilder, err := neo.NewQueryBuilder("create")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer driver.Close(ctx)
-
-
-	node, err := neo.CreateAndReturnNode(ctx, driver, "World", map[string]interface{}{
-		"name": body.Name,
-	})
-
+	
+	userIDInt, err := strconv.ParseInt(userID, 10, 64)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "invalid userID", http.StatusBadRequest)
 		return
 	}
+
+	query, params := queryBuilder.
+		Match("(u:User) WHERE u.userID = $userID").
+		With("u").
+		Create("(w:World {name: $name, type: $type, description: $description, userID: $userID})").
+		Create("(u)-[:OWNS]->(w)").
+		Return("w").
+		WithParam("name", world.Name).
+		WithParam("type", world.Type).
+		WithParam("description", world.Description).
+		WithParam("userID", userIDInt).
+		Build()
 
 	
+	fmt.Println(query)
+	fmt.Println(params)
+
+	node, err := createWorld(query, params)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(node)
+}
 
-
-		
+func GetWorld(w http.ResponseWriter, r *http.Request, rctx routing.Context) {
+	id := rctx.GetPathParam("id")
+	if id == "" {
+		http.Error(w, "missing id", http.StatusBadRequest)
+		return
 	}
+
+	fmt.Println(id)
+
+	queryBuilder, err := neo.NewQueryBuilder("match")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	query, params := queryBuilder.
+		Match("(w) WHERE elementId(w) = $elementId").
+		Return("w").
+		WithParam("elementId", id).
+		Build()
+
+	node, err := fetchWorld(query, params)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(node)
+}
+
+
+
+
+
